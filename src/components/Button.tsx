d1769acd455635d5b5a2a0877604198565119513
romannikiforov <escaro@ukr.net>
2024-11-02 12:49:22 +0200

    temp commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,25 @@
+import { useReducer } from 'react'
+
+const initialState = { value: false }
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case "show": return { value: true }
+    case "hide": return { value: false }
+    default: throw Error("not found any cases")
+  }
+}
+
 function Button() {
-  const value = true;
+  const [{ value }, dispatch] = useReducer(reducer, initialState)
 
   return (
     <>
       <div className="btn-group">
-        <button onClick={(f) => f} className="btn btn_primary">
+        <button onClick={() => dispatch({ type: "show" })} className="btn btn_primary">
           Show
         </button>
-        <button onClick={(f) => f} className="btn btn_default">
+        <button onClick={() => dispatch({ type: "hide" })} className="btn btn_default">
           Hide
         </button>
       </div>
