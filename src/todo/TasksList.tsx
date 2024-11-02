d6184b24257f5336e688a06ecabaac0d202aeb01
romannikiforov <escaro@ukr.net>
2024-11-02 11:14:27 +0200

    7. filter tasks

--- src/todo/TasksList.tsx
+++ src/todo/TasksList.tsx
@@ -10,10 +10,25 @@ type TasksListProps = {
     removeTask: (id: string) => Promise<void>,
 }
 
+function filterTasks(tasks: TaskItemType[], selectFilter: number, textFilter: string) {
+
+    if (textFilter) {
+        tasks = tasks.filter(t => t.title.toLowerCase().includes(textFilter.toLowerCase()))
+    }
+    if (selectFilter === 2) {
+        tasks = tasks.filter(t => t.done)
+    }
+    if (selectFilter === 3) {
+        tasks = tasks.filter(t => !t.done)
+    }
+
+    return tasks;
+}
+
+
 const TasksList = ({ tasks, toggleTask, removeTask }: TasksListProps) => {
     const [selectFilter, setSelectFilter] = useState(0);
     const [textFilter, setTextFilter] = useState('');
-
 
     const handleSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
         setSelectFilter(+e.target.value)
@@ -21,11 +36,13 @@ const TasksList = ({ tasks, toggleTask, removeTask }: TasksListProps) => {
 
     const handleTextChange = (val: string) => setTextFilter(val)
 
+    const filteredTasks = filterTasks(tasks, selectFilter, textFilter)
+
     return (
         <>
             <TaskFilter handleSelectChange={handleSelectChange} handleTextChange={handleTextChange} />
             <ul className="mb-5">
-                {tasks.map(task => <Task key={task.id} task={task}
+                {filteredTasks.map(task => <Task key={task.id} task={task}
                     toggleTask={toggleTask}
                     removeTask={removeTask}
 
